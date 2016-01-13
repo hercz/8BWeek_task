02789af76035583dc644b69a107e212f31bd807e
Herczku Misi <kmisku@gmail.com>
2016-01-13 23:28:24 +0100

    Error Code: 1064. You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'Ltd,Ora W. Cohen,Lorem ipsum dolor,185-4814 Morbi Av.,Częstochowa,SL,40366,Sene' at line 1

--- Classes/Customers.py
+++ Classes/Customers.py
@@ -9,25 +9,54 @@ cursor.execute("USE SANDBOX;")
 
 class Customers:
 
-    def __init__(self, CustomerID, CompanyName, ContactName, ContactTitle, Address, City,
-                 Region, PostalCode, Country, Phone, Fax):
-        self.CustomerID = CustomerID
-        self.CompanyName= CompanyName
-        self.ContactName = ContactName
-        self.ContactTitle= ContactTitle
-        self.Address = Address
-        self.City = City
-        self.Region = Region
-        self.PostalCode = PostalCode
-        self.Country =Country
-        self.Phone = Phone
-        self.Fax = Fax
 
     def persist(self):
-        pass
+        print("INSERT INTO customers VALUES ({},{},{},{},{},{},{},{},{},{},{});".format(self.CustomerID,
+                                                                                            self.CompanyName,
+                                                                          self.ContactName,self.ContactTitle,
+                                                                          self.Address, self.City, self.Region,
+                                                                          self.PostalCode, self.Country, self.Phone,
+                                                                          self.Fax))
 
+        cursor.execute("INSERT INTO customers VALUES ({},{},{},{},{},{},{},{},{},{},{});".format(self.CustomerID,
+                                                                                                    self.CompanyName,
+                                                                                  self.ContactName,self.ContactTitle,
+                                                                                  self.Address, self.City, self.Region,
+                                                                                  self.PostalCode, self.Country, self.Phone,
+                                                                                  self.Fax))
+        # database_connector.commit()
 
     @staticmethod
-    def parse():
-        pass
+    def parse(row):
+        parsed_row = row.split(";")
+        customers = Customers()
+        customers.CustomerID = parsed_row[0]
+        customers.CompanyName= parsed_row[1]
+        customers.ContactName = parsed_row[2]
+        customers.ContactTitle= parsed_row[3]
+        customers.Address = parsed_row[4]
+        customers.City = parsed_row[5]
+        customers.Region = parsed_row[6]
+        customers.PostalCode = parsed_row[7]
+        customers.Country =parsed_row[8]
+        customers.Phone = parsed_row[9]
+        customers.Fax = parsed_row[10]
+        return customers
 
+    @staticmethod
+    def data_reader(csv_file):
+        csv_rows = []
+        with open(csv_file, "r", encoding="utf8") as file:
+            for row in file:
+                csv_rows.append(row)
+        return csv_rows
+
+
+    def caller(self):
+        rows = Customers.data_reader("../CSV_FILES/customers.csv")
+        for i in range(1, len(rows)):
+            customers = Customers.parse(rows[i])
+            customers.persist()
+
+m = Customers()
+m.caller()
